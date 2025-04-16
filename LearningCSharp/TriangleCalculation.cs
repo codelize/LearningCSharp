4329c4198303f3d0604d5313ab6a278867e470b6
Leonardo Ferreira Lizier <leonardo.lizier@ituran.com.br>
2025-04-16 18:59:28 -0300

    add TriangleCalculation

---
+++ LearningCSharp/TriangleCalculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class TriangleCalculation
+{
+    public static void Executar()
+    {
+        double largura = 10.5;
+        double comprimento = 20.3;
+
+        double area = largura * comprimento;
+
+        Console.WriteLine($"A Área do terreno é: {area} metros quadrados.");
+
+    }
+}
+
