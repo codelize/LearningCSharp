caeb57c9f4cf71c738bfb8931af630bef58330d8
Leonardo Ferreira Lizier <leonardo.lizier@ituran.com.br>
2025-04-16 17:04:40 -0300

    Conversor de milhas em quilometros

---
+++ LearningCSharp/MilesProgram.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.Write("Digite a quantidade de milhas a serem convertidas: ");
+string milhasInput = Console.ReadLine();
+
+double milhas = double.TryParse(milhasInput, out double parsedMilhas) ? parsedMilhas : 0;
+
+double quilometros = milhas * 1.60934;
+Console.WriteLine($"O valor em quilometros é: {quilometros}");  
